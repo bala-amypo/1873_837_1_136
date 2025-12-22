b9f991a24553496b1c1b5094fe3f8f21692f4885
bala-amypo <bala@amypo.in>
2025-12-22 17:08:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,18 +1,30 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "users")   //  IMPORTANT FIX
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    // getters & setters
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String role;
 }
